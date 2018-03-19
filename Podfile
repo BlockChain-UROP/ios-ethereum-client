321c78ff55eb07149089c7929ce97e9a7d32f459
Tina Lee <tleeae@connect.ust.hk>
2018-03-20 00:00:16 +0800

    deployed on heroku and tested the get function

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'test' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for test
+
+# This is a combination of 3 commits.
+    pod 'Alamofire', '~> 4.6'
+end
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var input1: UITextField!
+    
+    
+    @IBAction func tina(_ sender: UIButton) {
+        // url request
+        let requestURL = "https://iodine.herokuapp.com/asset/info/"+input1.text!
+        Alamofire.request(requestURL).responseJSON { (response) in
+            if let JSON = response.result.value{
+                let parsedJSON = JSON as! NSDictionary
+                print(JSON)
+                
+                //example if there is an id
+                let asset = parsedJSON.object(forKey: "asset")!
+                let avail = (asset as AnyObject).object(forKey: "avail")!
+                let condition = (asset as AnyObject).object(forKey: "condition")!
+                let holder = (asset as AnyObject).object(forKey: "holder")!
+                let id = (asset as AnyObject).object(forKey: "id")!
+                let name = (asset as AnyObject).object(forKey: "name")!
+                let publisher = (asset as AnyObject).object(forKey: "publisher")!
 
+                self.label1.text = """
+                avail: \(avail)
+                condition: \(condition)
+                holder: \(holder)
+                ID: \(id)
+                name: \(name)
+                publisher: \(publisher)
+                """
+
+                
+            }
+        }
+    }
+    
+    
+    
 }
 
